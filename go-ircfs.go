79ab9bacfbfc55299047f9b51fcf4785f1ae1209
John Soros <johnny@9souldier.org>
2011-01-11 16:43:38 +0100

    gofmt'ed

--- go-ircfs.go
+++ go-ircfs.go
@@ -14,14 +14,14 @@ import (
 
 type Ctl struct {
 	srv.File
-	status *bytes.Buffer
+	status   *bytes.Buffer
 	networks map[string]*irc.Network
 }
 
 type Chan struct {
 	srv.File
 	network, name string
-	contents *bytes.Buffer
+	contents      *bytes.Buffer
 }
 
 var addr = flag.String("addr", ":5640", "network address")
@@ -29,64 +29,70 @@ var debug = flag.Bool("d", false, "print debug messages")
 
 
 func execCmd(ctl *Ctl, words []string) {
-	if(len(words)==0) { return }
+	if len(words) == 0 {
+		return
+	}
 	fmt.Fprintf(ctl.status, ">> %v\n", words)
 	switch words[0] {
 	case "connect":
 		connect(ctl, words)
 	case "join":
-		join(ctl,words)
+		join(ctl, words)
 	}
 }
 
 func connect(ctl *Ctl, words []string) {
 	var network, server, nick, username, realname, pass string
-	if len(words)<4 { return }
+	if len(words) < 4 {
+		return
+	}
 	network = words[1]
-	server  = words[2]
-	nick    = words[3]
-	if len(words)<5 {
+	server = words[2]
+	nick = words[3]
+	if len(words) < 5 {
 		username = nick
 	} else {
 		username = words[4]
 	}
-	if len(words)<6 {
+	if len(words) < 6 {
 		realname = username
 	} else {
 		realname = words[5]
 	}
-	if len(words)>6 {
+	if len(words) > 6 {
 		pass = words[6]
 	}
-	n := irc.NewNetwork(server,nick,username,realname,pass,"")
+	n := irc.NewNetwork(server, nick, username, realname, pass, "")
 	err := n.Connect()
 	if err != nil {
-		fmt.Fprintf(ctl.status, "<< %v\n",err)
+		fmt.Fprintf(ctl.status, "<< %v\n", err)
 		return
 	}
 	fmt.Fprintf(ctl.status, "<< ok\n")
 	ctl.networks[network] = n
 	ch := make(chan *irc.IrcMessage)
-	n.Listen.RegListener("*", "gircfs",ch)
+	n.Listen.RegListener("*", "gircfs", ch)
 	go iloop(ch)
 }
 
 func iloop(ch chan *irc.IrcMessage) {
 	for {
-		msg :=<- ch
-		fmt.Printf("!!! %v\n",msg)
+		msg := <-ch
+		fmt.Printf("!!! %v\n", msg)
 	}
 }
 
 func join(ctl *Ctl, words []string) {
-	if len(words)<3 || len(words)>4 {
+	if len(words) < 3 || len(words) > 4 {
 		fmt.Fprintf(ctl.status, "<< syntax: join <network> <channel> [optional-key]\n")
 		return
 	}
 	var key string
-	if len(words)==4 { key = words[3] }
+	if len(words) == 4 {
+		key = words[3]
+	}
 	net := ctl.networks[words[1]]
-	if net==nil {
+	if net == nil {
 		fmt.Fprintf(ctl.status, "<< invalid network\n")
 		return
 	}
@@ -95,20 +101,20 @@ func join(ctl *Ctl, words []string) {
 
 	c := new(Chan)
 	c.network = words[1]
-	c.name    = name
-	c.contents= new(bytes.Buffer)
-	err := c.Add(root, c.network + "_" + c.name, user, nil, 0666, ctl)
-	if err!=nil {
-		fmt.Fprintf(ctl.status, "<< %v\n",err)
+	c.name = name
+	c.contents = new(bytes.Buffer)
+	err := c.Add(root, c.network+"_"+c.name, user, nil, 0666, ctl)
+	if err != nil {
+		fmt.Fprintf(ctl.status, "<< %v\n", err)
 		return
 	}
-	fmt.Fprintf(ctl.status,"<< ok\n")
+	fmt.Fprintf(ctl.status, "<< ok\n")
 
 }
 func chanName(name string) string {
 	c0 := name[0]
-	if c0 != '#' && c0 != '&' && c0!='+' && c0!='!' {
-		name = "#"+name
+	if c0 != '#' && c0 != '&' && c0 != '+' && c0 != '!' {
+		name = "#" + name
 	}
 	return name
 }
@@ -116,13 +122,13 @@ func chanName(name string) string {
 
 func (ctl *Ctl) Write(fid *srv.FFid, data []byte, offset uint64) (int, *p.Error) {
 	if offset > 0 {
-		return 0,&p.Error{"Long writes not supported", 0}
+		return 0, &p.Error{"Long writes not supported", 0}
 	}
 	go func() {
-	lines := strings.Split(string(data),"\n",-1)
-	for _,line := range lines {
-		execCmd(ctl, strings.Fields(line))
-	}
+		lines := strings.Split(string(data), "\n", -1)
+		for _, line := range lines {
+			execCmd(ctl, strings.Fields(line))
+		}
 	}()
 	return len(data), nil
 }
@@ -132,7 +138,9 @@ func (ctl *Ctl) Read(fid *srv.FFid, buf []byte, offset uint64) (int, *p.Error) {
 	b := ctl.status.Bytes()[offset:]
 	copy(buf, b)
 	l := len(b)
-	if len(buf) < l { l = len(buf) }
+	if len(buf) < l {
+		l = len(buf)
+	}
 	return l, nil
 }
 
@@ -165,7 +173,7 @@ func main() {
 
 	s.Start(s)
 	err = s.StartNetListener("tcp", *addr)
-	if err!=nil {
+	if err != nil {
 		goto error
 	}
 	return
